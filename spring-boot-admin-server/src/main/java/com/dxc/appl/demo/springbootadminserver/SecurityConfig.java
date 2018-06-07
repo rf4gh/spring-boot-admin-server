2ac279eddd9370a3f75e407b4ada83f2d0c874fc
rferstl <rferstl@dxc.com>
2018-06-07 17:46:03 +0200

    SBA server now with correct security config

---
+++ spring-boot-admin-server/src/main/java/com/dxc/appl/demo/springbootadminserver/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.dxc.appl.demo.springbootadminserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+
+/**
+ * see: http://codecentric.github.io/spring-boot-admin/2.0.0/#_securing_spring_boot_admin_server
+ *      https://github.com/codecentric/spring-boot-admin/blob/master/spring-boot-admin-samples/spring-boot-admin-sample-servlet/src/main/java/de/codecentric/boot/admin/SpringBootAdminApplication.java
+ *      
+ * @author rferstl
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
+
+    public SecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        // add basicAuth for REST client
+        http
+        	.cors()
+        .and()
+        	.authorizeRequests()
+            	.antMatchers(adminContextPath + "/assets/**").permitAll()
+            	.antMatchers(adminContextPath + "/login").permitAll()
+            	.anyRequest().authenticated()
+        .and()
+        	.formLogin()
+        		.loginPage(adminContextPath + "/login").successHandler(successHandler)
+        .and()
+        	.logout().logoutUrl(adminContextPath + "/logout")
+        .and()
+        	.httpBasic()
+        .and()
+        	.csrf().disable();
+        super.configure(http);
+    }
+}
--- spring-boot-admin-server/src/main/java/com/dxc/appl/demo/springbootadminserver/SpringBootAdminServerApplication.java
+++ spring-boot-admin-server/src/main/java/com/dxc/appl/demo/springbootadminserver/SpringBootAdminServerApplication.java
@@ -3,11 +3,12 @@ package com.dxc.appl.demo.springbootadminserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-
 @Configuration
+@Import({ SecurityConfig.class })
 @EnableAutoConfiguration
 @EnableAdminServer
 public class SpringBootAdminServerApplication {
@@ -15,17 +16,4 @@ public class SpringBootAdminServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminServerApplication.class, args);
 	}
-	
-//	private final String adminContextPath="";
-	
-//	@Bean
-//	public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
-//		// @formatter:off
-//		return http.authorizeExchange().pathMatchers(adminContextPath + "/assets/**").permitAll()
-//				.pathMatchers(adminContextPath + "/login").permitAll().anyExchange().authenticated().and().formLogin()
-//				.loginPage(adminContextPath + "/login").and().logout().logoutUrl(adminContextPath + "/logout").and()
-//				.httpBasic().and().csrf().disable().build();
-//		// @formatter:on
-//	}
-	
 }
